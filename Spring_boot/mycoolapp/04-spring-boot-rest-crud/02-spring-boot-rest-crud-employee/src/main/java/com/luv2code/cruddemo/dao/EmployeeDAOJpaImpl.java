5fb411ec6ea5ad6003eacc229bbf2755c684a66d
vinodh <sagilivinodh1@gmail.com>
2023-08-13 16:11:22 +0530

    add files

--- Spring_boot/mycoolapp/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ Spring_boot/mycoolapp/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -1,6 +1,6 @@
-package com.luv2code.springboot.cruddemo.dao;
+package com.luv2code.cruddemo.dao;
 
-import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.cruddemo.entity.Employee;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
--- Spring_boot/mycoolapp/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
+++ Spring_boot/mycoolapp/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
@@ -1,13 +1,17 @@
-package com.luv2code.springboot.cruddemo.rest;
+package com.luv2code.cruddemo.rest;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
-import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.cruddemo.dao.EmployeeDAO;
+import com.luv2code.cruddemo.entity.Employee;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/api")
+
+@RestController
+@RequestMapping("/api")
 public class EmployeeRestController {
     private EmployeeDAO employeeDAO;
 //    quick and dirty: inject   dao
